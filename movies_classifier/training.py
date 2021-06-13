e1e3744e634fe4cf420bb23fd765d68e64205456
amarrerod <alemarrerodiaz@gmail.com>
2021-06-13 08:52:10 +0200

    💥  Replaces RandomForest for LR

--- movies_classifier/training.py
+++ movies_classifier/training.py
@@ -1,4 +1,5 @@
-from sklearn.ensemble import RandomForestRegressor
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
 import numpy as np
 from movies_classifier.utilities import save_model
@@ -6,16 +7,15 @@ from movies_classifier.utilities import save_model
 
 def create_and_train_model(X, y):
     """
-    Crea un modelo RandomForestRegressor y lo entrena con los datos recibidos
+    Crea un modelo Linear Regressor y lo entrena con los datos recibidos
     Además lo guarda en el directorio models para usos posteriores
     """
-    n_trees = 894
-    forest = RandomForestRegressor(n_estimators=n_trees)
-    scores = cross_val_score(forest, X, y, scoring="neg_mean_squared_error", cv=3)
-    forest_rmse = np.sqrt(-scores)
+    reg = LinearRegression()
+    scores = cross_val_score(reg, X, y, scoring="neg_mean_squared_error", cv=3)
+    reg_rmse = np.sqrt(-scores)
     print(f"Training done with the following resuñts")
-    print(f"Scores: {forest_rmse}")
+    print(f"Scores: {reg_rmse}")
     print(f"Mean: {scores.mean()}")
     print(f"STD: {scores.std()}")
-    save_model(forest, "movies_classifier/models/forest_894.pkl")
-    return forest
+    save_model(reg, "movies_classifier/models/reg.pkl")
+    return reg
