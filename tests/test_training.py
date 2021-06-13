3653b10c268567493cd8c0c288ea6856dd9b809a
amarrerod <alemarrerodiaz@gmail.com>
2021-06-13 09:08:39 +0200

    ✅ Updates tests to LR model

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,12 +1,12 @@
+from movies_classifier.keys import REG_MODEL
 from movies_classifier.training import create_and_train_model
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from movies_classifier.preprocessing import create_datasets
 from os.path import isfile
 
 
 def test_create_and_train_model():
     X_train, _, y_train, _ = create_datasets()
-    forest = create_and_train_model(X_train, y_train)
-    assert type(forest) == type(RandomForestRegressor())
-    assert forest.n_estimators == 894
-    assert isfile("movies_classifier/models/forest_894.pkl")
+    model = create_and_train_model(X_train, y_train)
+    assert type(model) == type(LinearRegression())
+    assert isfile(REG_MODEL)
