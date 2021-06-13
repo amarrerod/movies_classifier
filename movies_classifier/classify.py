bdcfe7e255c3f00b6dc45957d9251d679c76e3df
amarrerod <alemarrerodiaz@gmail.com>
2021-06-13 09:07:32 +0200

    🚧  Creates the prepare_data function

--- movies_classifier/classify.py
+++ movies_classifier/classify.py
@@ -4,4 +4,11 @@ from movies_classifier.keys import REG_MODEL
 
 
 def classify(X: np.array) -> float:
+    """
+    Predice la clasificación de X con el modelo de Regresión Lineal
+    """
     return load_model(REG_MODEL).predict(X)
+
+
+def prepare_data(X: dict) -> np.array:
+    pass
